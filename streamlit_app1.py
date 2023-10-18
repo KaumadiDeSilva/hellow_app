c7cd98b0157ffc12c602334a98ac85562545d9ee
KaumadiDeSilva <128453835+KaumadiDeSilva@users.noreply.github.com>
2023-10-18 13:16:37 +0530

    Update streamlit_app1.py

--- streamlit_app1.py
+++ streamlit_app1.py
@@ -1,4 +1,4 @@
-import streamlit as 
+import streamlit as st
 import machine_learning as ml
 import feature_extraction as fe
 from bs4 import BeautifulSoup
@@ -7,11 +7,11 @@ import matplotlib.pyplot as plt
 
 st.title('Phishing Website Detection using Machine Learning')
 
-st.write('This ML-based app is developed for educational purposes. Objective of the app is detecting phishing websites only using content data. Not URL!'
-         ' You can see the details of approach, data set, and feature set if you click on _"See The Details"_. ')
+st.write('This ML-based app is developed for educational purposes. The objective of the app is to detect phishing websites using only content data, not the URL.'
+         ' You can see the details of the approach, data set, and feature set if you click on _"See The Details"_. ')
 
 st.subheader('Data set')
 st.write('I used _"phishtank.org"_ & _"tranco-list.eu"_ as data sources.')
 st.write('Totally 26584 websites ==> **_16060_ legitimate** websites | **_10524_ phishing** websites')
-st.write('Data set was created in October 2022.')
+st.write('The data set was created in October 2022.')
 
