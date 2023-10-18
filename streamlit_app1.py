4b7df473bf112a9537a03088c1def73624bdb6cc
KaumadiDeSilva <128453835+KaumadiDeSilva@users.noreply.github.com>
2023-10-18 13:21:29 +0530

    Update streamlit_app1.py

--- streamlit_app1.py
+++ streamlit_app1.py
@@ -6,8 +6,14 @@ st.title('Phishing Website Detection using Machine Learning')
 st.write('This ML-based app is developed for educational purposes. The objective of the app is to detect phishing websites using only content data, not the URL.'
          ' You can see the details of the approach, data set, and feature set if you click on _"See The Details"_. ')
 
-st.subheader('Data set')
-st.write('I used _"phishtank.org"_ & _"tranco-list.eu"_ as data sources.')
-st.write('Totally 26584 websites ==> **_16060_ legitimate** websites | **_10524_ phishing** websites')
-st.write('The data set was created in October 2022.')
+st.title('Phishing Website Detection using Machine Learning')
+st.write('This ML-based app is developed for educational purposes. Objective of the app is detecting phishing websites only using content data. Not URL!'
+         ' You can see the details of approach, data set, and feature set if you click on _"See The Details"_. ')
 
+
+with st.expander("PROJECT DETAILS"):
+     st.subheader('Approach')
+     st.write('I used _supervised learning_ to classify phishing and legitimate websites. '
+             'I benefit from content-based approach and focus on html of the websites. '
+             'Also, I used scikit-learn for the ML models.'
+             )
