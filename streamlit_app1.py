694cb20e6792d726895a84cd53ec96f0bd2bd564
KaumadiDeSilva <128453835+KaumadiDeSilva@users.noreply.github.com>
2023-10-18 13:24:37 +0530

    Update streamlit_app1.py

--- streamlit_app1.py
+++ streamlit_app1.py
@@ -2,13 +2,8 @@ import streamlit as st
 
 
 st.title('Phishing Website Detection using Machine Learning')
-
 st.write('This ML-based app is developed for educational purposes. The objective of the app is to detect phishing websites using only content data, not the URL.'
          ' You can see the details of the approach, data set, and feature set if you click on _"See The Details"_. ')
-
-st.title('Phishing Website Detection using Machine Learning')
-st.write('This ML-based app is developed for educational purposes. Objective of the app is detecting phishing websites only using content data. Not URL!'
-         ' You can see the details of approach, data set, and feature set if you click on _"See The Details"_. ')
 
 with st.expander("PROJECT DETAILS"):
      st.subheader('Approach')
