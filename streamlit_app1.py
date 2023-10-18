5848b52fdcca79f9518b8f1c6a5ddb5281c2cec2
KaumadiDeSilva <128453835+KaumadiDeSilva@users.noreply.github.com>
2023-10-18 13:08:02 +0530

    Update streamlit_app1.py

--- streamlit_app1.py
+++ streamlit_app1.py
@@ -6,5 +6,8 @@ st.write('This ML-based app is developed for educational purposes. Objective of 
          ' You can see the details of approach, data set, and feature set if you click on _"See The Details"_. ')
 
 
-
+         st.subheader('Data set')
+         st.write('I used _"phishtank.org"_ & _"tranco-list.eu"_ as data sources.')
+         st.write('Totally 26584 websites ==> **_16060_ legitimate** websites | **_10524_ phishing** websites')
+         st.write('Data set was created in October 2022.')
       
