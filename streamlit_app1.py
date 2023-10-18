5318582218d23a74781241b0328defc3bc8dc7c8
KaumadiDeSilva <128453835+KaumadiDeSilva@users.noreply.github.com>
2023-10-18 12:45:28 +0530

    Update streamlit_app1.py

--- streamlit_app1.py
+++ streamlit_app1.py
@@ -5,8 +5,6 @@ st.title('Phishing Website Detection using Machine Learning')
 st.write('This ML-based app is developed for educational purposes. Objective of the app is detecting phishing websites only using content data. Not URL!'
          ' You can see the details of approach, data set, and feature set if you click on _"See The Details"_. ')
 
-with st.echo('approach'):
-         I used supervised learning to classify phishing and legitimate websites. I benefit from content-based approach and focus on html of the websites. Also, I used scikit-learn for the ML models.
 
 
       
