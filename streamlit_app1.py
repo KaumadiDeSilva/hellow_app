86f04df92d5bb584290dcc4a135f04587391fe51
KaumadiDeSilva <128453835+KaumadiDeSilva@users.noreply.github.com>
2023-10-18 13:17:20 +0530

    Update streamlit_app1.py

--- streamlit_app1.py
+++ streamlit_app1.py
@@ -1,9 +1,5 @@
 import streamlit as st
-import machine_learning as ml
-import feature_extraction as fe
-from bs4 import BeautifulSoup
-import requests as re
-import matplotlib.pyplot as plt
+
 
 st.title('Phishing Website Detection using Machine Learning')
 
